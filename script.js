6006ae800cba9597e35d6041816dbc0720e3ed76
peep <sokolenok220@gmail.com>
2023-12-25 23:45:20 +0300

    first-final

---
+++ script.js
@@ -0,0 +1,25 @@
+'use strict'
+
+
+let getWord = prompt('Введите слово','');
+
+function Vowels(word) {
+    let letters = ['О', 'У', 'Ы', 'Э', 'Е', 'Ё', 'И', 'Ю', 'Я','А', 'а', 'о', 'у', 'ы', 'э', 'е', 'ё', 'и', 'ю', 'я'];
+    let numLetters = 0;
+
+    for(let i = 0; i < letters.length; i++) {
+        const checkLetters = letters[i];
+        
+        for(let x = 0; x < word.length; x++) {
+            const checkWord = word[x];
+            if(checkWord == checkLetters) {
+                numLetters++;
+            }
+        }
+    }
+    
+    console.log(numLetters);
+}
+
+Vowels(getWord);
+
